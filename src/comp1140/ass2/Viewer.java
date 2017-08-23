033654e0d769abe1f49a2b73481f3bab7386654b
cup <u6210090@anu.edu.au>
2017-08-23 16:48:04 +1000

    modify the viewer

--- src/comp1140/ass2/Viewer.java
+++ src/comp1140/ass2/Viewer.java
@@ -1,6 +1,7 @@
 package comp1140.ass2;
 
 import javafx.application.Application;
+import javafx.beans.property.ObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
@@ -8,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -61,23 +63,22 @@ public class Viewer extends Application {
         hb.setLayoutY(VIEWER_HEIGHT - 50);
         controls.getChildren().add(hb);
         root.getChildren().add(controls);
+
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Patchwork Viewer");
         Scene scene = new Scene(root, VIEWER_WIDTH, VIEWER_HEIGHT);
-
         makeControls();
-
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     //describe the window attributes
-    public void canvas(){
-
-    }
+//    public void canvas(){
+//
+//    }
 
     // show the final window when the game ends.Including two players'
     // socres and who wins the game. Show a restart button as well.
@@ -86,13 +87,17 @@ public class Viewer extends Application {
     }
 
     //displays the squilt board for player 1.
-    public void squiltBoard1(){}
+    public void squiltBoard1(){
+
+    }
 
     //displays the squilt board for player 2.
     public void squiltBoard2(){}
 
     //displays the time board for players.
-    public void timeBoard(){}
+    public void timeBoard(){
+        ObjectProperty<Image> tb ;
+    }
 
     //displays the time token of player1,
     // call the drag() when token is dragged.
