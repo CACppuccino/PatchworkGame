e647a9df4cc7d2930c49c8055efe0783f2a0ef50
Adonis <u6385898>
2017-09-27 16:58:36 +1000

    ai ish

--- src/comp1140/ass2/PatchworkAI.java
+++ src/comp1140/ass2/PatchworkAI.java
@@ -8,10 +8,10 @@ import static java.lang.Math.min;
 public class PatchworkAI {
     private final PatchworkGame game;
 
-    final static char[] PATCHES = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W',
-            'X','Y','Z','a','b','c','d','e','f','g'};
-    public static char [] three;
-    public static String [] validTileStr;
+//    final static char[] PATCHES = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W',
+//            'X','Y','Z','a','b','c','d','e','f','g'};
+//    public static char [] three;
+//    public static String [] validTileStr;
     public PatchworkAI(PatchworkGame game) {
         this.game = game;
     }
@@ -24,76 +24,108 @@ public class PatchworkAI {
      */
     public static String generatePatchPlacement(String patchCircle, String placement) {
         // FIXME Task 10: generate a valid move
-        String vaildPlacement = "";
-        State p1 = new State(1);
-        State p2 = new State(2);
-        int aPosition = 0;
+        patchCircle=patchCircle.substring(patchCircle.indexOf('A')+1)+patchCircle.substring(0,patchCircle.indexOf('A')-1);
+        PatchworkGame.three=patchCircle.substring(0,3).toCharArray();
+        PatchworkGame.isPlacementValid(patchCircle,placement);
+        State p = State.check_turn(PatchworkGame.p1, PatchworkGame.p2) == 1 ? PatchworkGame.p1 : PatchworkGame.p2;
+        String choices = ".";
+        for (char c : PatchworkGame.three) {
+            int n = c > 'Z' ? c - 'a' : c - 'A';
+            if (State.affordPartch(p.buttonCount, PatchworkGame.tileCost[n])) choices += c;
+        }
         Random r = new Random();
-        ArrayList<Character> newPatchCircle = new ArrayList<>();
-        ArrayList<String> validTileString = new ArrayList<>();
-        for (int j = 0; j < patchCircle.length();j++)
-            newPatchCircle.add(patchCircle.charAt(j));
-        for (int i=0;i<patchCircle.length();i++)
-            if (patchCircle.charAt(i)=='A')
-                aPosition=i+1;
-        three = new char[] {newPatchCircle.get(aPosition%newPatchCircle.size()), newPatchCircle.get((aPosition + 1) % newPatchCircle.size()), newPatchCircle.get((aPosition + 2) % newPatchCircle.size())};
-        int [] tileCost = new int[three.length];
-        for ( int i = 0; i < three.length; i++)
-            for ( int j = 0; j < PATCHES.length;j++)
-                if ( PATCHES[j] == three[i])
-                    tileCost[i] = PatchworkGame.tileCost[j];
-
-        int minimalCost = min(min(tileCost[0],tileCost[1]),tileCost[2]);
-        if ( State.check_turn(p1,p2) == 1)
-            if ( minimalCost - p1.buttonCount < 0)
-                vaildPlacement = ".";
-            else {
-                String threeTile = new String(three);
-                validTileString = vaildPatchStirng(placement, threeTile);
-
-                if (validTileString.size() == 0) return vaildPlacement = ".";
-                int indexAdd = r.nextInt(validTileString.size());
-                vaildPlacement = (validTileString.get(indexAdd));
+        char t = choices.charAt(r.nextInt(choices.length()));
+        if (t=='.') return ".";
+        State a=PatchworkGame.p1,b=PatchworkGame.p2;
+        int c = PatchworkGame.aPlc;
+        char[] d = PatchworkGame.three;
+        for (char i = 'A'; i < 'J'; i++) {
+            for (char j = 'A'; j < 'J'; j++) {
+                for (char k = 'A'; k < 'I'; k++) {
+                    if (PatchworkGame.isPlacementValid(patchCircle, placement + t + i + j + k)) {
+                        return "" + t + i + j + k;
+                    }
+                    PatchworkGame.p1=a;
+                    PatchworkGame.p2=b;
+                    PatchworkGame.aPlc=c;
+                    PatchworkGame.three=d;
+                }
             }
-        else
-            if (minimalCost - p2.buttonCount < 0)
-                vaildPlacement = ".";
-            else {
-                String threeTile = new String(three);
-                validTileString = vaildPatchStirng(placement, threeTile);
-
-                if (validTileString.size() == 0) return vaildPlacement = ".";
-                int indexAdd = r.nextInt(validTileString.size());
-                vaildPlacement = validTileString.get(indexAdd);
-            }
-        validTileStr = (String[])validTileString.toArray(new String[validTileString.size()]);
-        return vaildPlacement;
+        }
+        return "";
     }
 
-    // this method is to take the valid patch string from the patch placement
-    public static ArrayList<String> vaildPatchStirng(String placement, String patches){
-        ArrayList<String> validPatches = new ArrayList<>();
-        for (int i = 0; i < patches.length();i++)
-            for ( int j = 0; j < placement.length();j+=4)
-                if(placement.charAt(j) == patches.charAt(i))
-                    validPatches.add(placement.substring(j,j+3));
-//        String ans = String.join(",",(String[])validPatches.toArray(new String[validPatches.size()]));
-        return validPatches;
-    }
 
-    //choose to move the time token or buy a patch
-    public static int choose(){
-        return 1;
-    }
-
-    //evaluate the decision and get the best choice
-    public static void main(String[] args) {
-
-    }
-
-    //after the decision, execute should be called to use APIs provided by the game to
-    //interact with the game
-    public static void execute(){
-
-    }
+//        String vaildPlacement = "";
+//        State p1 = new State(1);
+//        State p2 = new State(2);
+//        int aPosition = 0;
+//        Random r = new Random();
+//        ArrayList<Character> newPatchCircle = new ArrayList<>();
+//        ArrayList<String> validTileString = new ArrayList<>();
+//        for (int j = 0; j < patchCircle.length();j++)
+//            newPatchCircle.add(patchCircle.charAt(j));
+//        for (int i=0;i<patchCircle.length();i++)
+//            if (patchCircle.charAt(i)=='A')
+//                aPosition=i+1;
+//        three = new char[] {newPatchCircle.get(aPosition%newPatchCircle.size()), newPatchCircle.get((aPosition + 1) % newPatchCircle.size()), newPatchCircle.get((aPosition + 2) % newPatchCircle.size())};
+//        int [] tileCost = new int[three.length];
+//        for ( int i = 0; i < three.length; i++)
+//            for ( int j = 0; j < PATCHES.length;j++)
+//                if ( PATCHES[j] == three[i])
+//                    tileCost[i] = PatchworkGame.tileCost[j];
+//
+//        int minimalCost = min(min(tileCost[0],tileCost[1]),tileCost[2]);
+//        if ( State.check_turn(p1,p2) == 1)
+//            if ( minimalCost - p1.buttonCount < 0)
+//                vaildPlacement = ".";
+//            else {
+//                String threeTile = new String(three);
+//                validTileString = vaildPatchStirng(placement, threeTile);
+//
+//                if (validTileString.size() == 0) return vaildPlacement = ".";
+//                int indexAdd = r.nextInt(validTileString.size());
+//                vaildPlacement = (validTileString.get(indexAdd));
+//            }
+//        else
+//            if (minimalCost - p2.buttonCount < 0)
+//                vaildPlacement = ".";
+//            else {
+//                String threeTile = new String(three);
+//                validTileString = vaildPatchStirng(placement, threeTile);
+//
+//                if (validTileString.size() == 0) return vaildPlacement = ".";
+//                int indexAdd = r.nextInt(validTileString.size());
+//                vaildPlacement = validTileString.get(indexAdd);
+//            }
+//        validTileStr = (String[])validTileString.toArray(new String[validTileString.size()]);
+//        return vaildPlacement;
+//    }
+//
+//    // this method is to take the valid patch string from the patch placement
+//    public static ArrayList<String> vaildPatchStirng(String placement, String patches){
+//        ArrayList<String> validPatches = new ArrayList<>();
+//        for (int i = 0; i < patches.length();i++)
+//            for ( int j = 0; j < placement.length();j+=4)
+//                if(placement.charAt(j) == patches.charAt(i))
+//                    validPatches.add(placement.substring(j,j+3));
+////        String ans = String.join(",",(String[])validPatches.toArray(new String[validPatches.size()]));
+//        return validPatches;
+//    }
+//
+//    //choose to move the time token or buy a patch
+//    public static int choose(){
+//        return 1;
+//    }
+//
+//    //evaluate the decision and get the best choice
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    //after the decision, execute should be called to use APIs provided by the game to
+//    //interact with the game
+//    public static void execute(){
+//
+//    }
 }
--- tests/comp1140/ass2/MatrixTest.java
+++ tests/comp1140/ass2/MatrixTest.java
@@ -19,18 +19,18 @@ public class MatrixTest {
             a = new Matrix(rng.nextInt(7), rng.nextInt(7));
             a.setZero();
 
-            for (double[] c:a.matrix)
-                for (double cc:c)
-                    assertTrue("matrix should be zero",cc==0);
+            for (double[] c : a.matrix)
+                for (double cc : c)
+                    assertTrue("matrix should be zero", cc == 0);
         }
     }
 
     @Test
     public void setMatrix() throws Exception {
-        Matrix a = new Matrix(rng.nextInt(50)+1,rng.nextInt(50)+1);
-        double get,ele ;
-        int r,c;
-        for (int i=0;i<TESTROUNDS;i++) {
+        Matrix a = new Matrix(rng.nextInt(50) + 1, rng.nextInt(50) + 1);
+        double get, ele;
+        int r, c;
+        for (int i = 0; i < TESTROUNDS; i++) {
             ele = rng.nextDouble();
             r = rng.nextInt(a.row);
             c = rng.nextInt(a.col);
@@ -44,78 +44,79 @@ public class MatrixTest {
     @Test
     public void addDiffMatrix() throws Exception {
         Matrix a, b, c;
-        a = new Matrix(3,3);
-        b = new Matrix(5,5);
+        a = new Matrix(3, 3);
+        b = new Matrix(5, 5);
         try {
-            c = Matrix.addMatrix(a,b);
-        }catch (Exception e){
-            assertTrue("didn't throw an exception when different size of matrix are added",e.getMessage().equals("Matrix not the same size"));
+            c = Matrix.addMatrix(a, b);
+        } catch (Exception e) {
+            assertTrue("didn't throw an exception when different size of matrix are added", e.getMessage().equals("Matrix not the same size"));
         }
 
     }
+
     @Test
-    public void addMatrix() throws Exception{
-        Matrix a,b,c;
-        double el1,el2;
-        for (int i=0;i<TESTROUNDS;i++){
+    public void addMatrix() throws Exception {
+        Matrix a, b, c;
+        double el1, el2;
+        for (int i = 0; i < TESTROUNDS; i++) {
             el1 = rng.nextDouble();
             el2 = rng.nextDouble();
-            a = new Matrix(3,3,el1);
-            b = new Matrix(3,3,el2);
-            c = Matrix.addMatrix(a,b);
-            for (double[] jj:c.matrix)
-                for (double jjj:jj)
-                    assertTrue("wrong addition on matrix, should be "+(el1+el2)+"but is "+jjj,jjj==el1+el2);
+            a = new Matrix(3, 3, el1);
+            b = new Matrix(3, 3, el2);
+            c = Matrix.addMatrix(a, b);
+            for (double[] jj : c.matrix)
+                for (double jjj : jj)
+                    assertTrue("wrong addition on matrix, should be " + (el1 + el2) + "but is " + jjj, jjj == el1 + el2);
         }
     }
 
     @Test
     public void addByDiffMatrix() throws Exception {
-        Matrix a = new Matrix(3,3),b = new Matrix(4,3);
+        Matrix a = new Matrix(3, 3), b = new Matrix(4, 3);
         try {
             a.addBy(b);
-        }catch (Exception e) {
+        } catch (Exception e) {
             assertTrue("didn't throw exception when matrix is added in wrong size", e.getMessage().equals("Matrix not the same size"));
         }
     }
 
     @Test
-    public void addByMatrix() throws Exception{
-        Matrix a = new Matrix(3,3,3),b = new Matrix(3,3,4);
+    public void addByMatrix() throws Exception {
+        Matrix a = new Matrix(3, 3, 3), b = new Matrix(3, 3, 4);
         Matrix c;
-        c = Matrix.addMatrix(a,b);
+        c = Matrix.addMatrix(a, b);
         a.addBy(b);
-        for (int i = 0;i<a.row;i++)
-            for (int j=0;j<a.col;j++)
-                assertTrue ("not equal when using addBy function",a.matrix[i][j]==c.matrix[i][j]);
+        for (int i = 0; i < a.row; i++)
+            for (int j = 0; j < a.col; j++)
+                assertTrue("not equal when using addBy function", a.matrix[i][j] == c.matrix[i][j]);
     }
 
     @Test
-    public void TestTranspose() throws Exception{
+    public void TestTranspose() throws Exception {
         int r;
         double ele;
-        for (int i=0;i<TESTROUNDS;i++){
+        for (int i = 0; i < TESTROUNDS; i++) {
             r = rng.nextInt(7);
-            Matrix a = new Matrix(r,r);
-            Matrix b = new Matrix(r,r);
-            for (int j = 0; j < r;j++){
-                for ( int k = 0; k < r;k++){
+            Matrix a = new Matrix(r, r);
+            Matrix b = new Matrix(r, r);
+            for (int j = 0; j < r; j++) {
+                for (int k = 0; k < r; k++) {
                     ele = rng.nextDouble();
                     a.matrix[j][k] = ele;
                     b.matrix[k][j] = ele;
                 }
             }
-            assertTrue ("The transpose of a  " + Arrays.deepToString(a.transpose().matrix) + " is not equal to b " + Arrays.deepToString(b.matrix) ,Arrays.deepEquals(a.transpose().matrix,b.matrix));
+            assertTrue("The transpose of a  " + Arrays.deepToString(a.transpose().matrix) + " is not equal to b " + Arrays.deepToString(b.matrix), Arrays.deepEquals(a.transpose().matrix, b.matrix));
         }
     }
 
     @Test
-    public void TestIsquare() throws Exception{
-        int c,r;
-        for ( int i = 0;i < TESTROUNDS;i++){
+    public void TestIsquare() throws Exception {
+        int c, r;
+        for (int i = 0; i < TESTROUNDS; i++) {
             c = rng.nextInt(7);
             r = rng.nextInt(7);
-            Matrix a = new Matrix(c,r);
+            Matrix a = new Matrix(c, r);
             System.out.println(r);
             System.out.println(c);
             System.out.println(a.isSquare());
@@ -125,4 +126,58 @@ public class MatrixTest {
 
     }
 
+
+    @Test
+    public void TestSubMatrix() throws Exception {
+        for (int i = 0; i < TESTROUNDS; i++) {
+            int r = rng.nextInt(6) + 1;
+            Matrix a = new Matrix(r, r);
+            Matrix b = new Matrix(r - 1, r - 1);
+            int er = rng.nextInt(r);
+            int ec = rng.nextInt(r);
+            int x = -1;
+            for (int j = 0; j < r; j++) {
+                if (j == er) {
+                    for (int k = 0; k < r; k++) {
+                        a.matrix[j][k] = rng.nextDouble();
+                    }
+                } else {
+                    x++;
+                    int y = -1;
+                    for (int k = 0; k < r; k++) {
+                        if (k == ec) a.matrix[j][k] = rng.nextDouble();
+                        else {
+                            double ele = rng.nextDouble();
+                            a.matrix[j][k] = ele;
+                            b.matrix[x][++y] = ele;
+                        }
+                    }
+                }
+            }
+            assertTrue("Incorrect Submatrix", (Matrix.createSubMatrix(a, er, ec)).equals(b));
+        }
+    }
+
+    public double det(Matrix m) throws Exception {
+        if (m.row == 1) return m.matrix[0][0];
+        double d = 0;
+        for (int i = 0; i < m.row; i++) {
+            d += m.matrix[i][0] * Math.pow(-1, i) * det(Matrix.createSubMatrix(m, i, 0));
+        }
+        return d;
+    }
+
+    @Test
+    public void TestDeterminant() throws Exception{
+        for (int i=0;i<TESTROUNDS;i++) {
+            int r = rng.nextInt(7);
+            Matrix a = new Matrix(r, r);
+            for (int j = 0; j < r; j++) {
+                for (int k = 0; k < r; k++) {
+                    a.matrix[j][k] = rng.nextDouble();
+                }
+            }
+            assertTrue("Incorrect Determinant", det(a)==Matrix.determinant(a));
+        }
+    }
 }
