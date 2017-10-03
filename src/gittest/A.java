9e0ef5cd1fec4f77dc5aa16aa8a70c81ef5ca6e1
cup <u6210090@anu.edu.au>
2017-10-03 17:59:21 +1100

    rmove useless part

--- src/gittest/A.java
+++
@@ -1,9 +0,0 @@
-package gittest;
-
-public class A {
-    @Override
-    public String toString(){
-        int a;
-        return "";
-    }
-}
--- src/gittest/C.java
+++
@@ -1,8 +0,0 @@
-package gittest;
-
-public class C {
-    @Override
-    public String toString(){
-        return "";
-    }
-}
