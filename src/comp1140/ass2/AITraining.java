cac0e8d4967545ecb2a9ce1041dcdd48284cdfc1
cup <u6210090@anu.edu.au>
2017-10-19 18:56:23 +1100

    activation functions added

--- src/comp1140/ass2/AITraining.java
+++ src/comp1140/ass2/AITraining.java
@@ -1,12 +1,25 @@
 package comp1140.ass2;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Random;
+
+import static comp1140.ass2.PatchworkAI.generateAllPositionPlacement;
 
 
 public class AITraining {
     static final int ROUNDS = 5000;
     static final int LAYERS = 3;
+    static Matrix[] WEIGHTS = new Matrix[LAYERS];
+    static {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("./model/t01_300.csv"));
+            String line;
+            while ((line=br.readLine())!=null && !line.isEmpty()){
+
+            }
+        }catch (Exception e){e.printStackTrace();}
+    }
     public double loss(){
         return 0;
     }
@@ -210,6 +223,7 @@ public class AITraining {
                 String plc = new String(), plc2 = new String();
                 int x = 0;
                 while (x<placement.length()) {
+                    boolean xx=PatchworkGame.isPlacementValid(patchCircle, plc2);
                     if (x<placement.length()) {
                         if (placement.charAt(x) == '.') {
                             x++;
@@ -220,7 +234,7 @@ public class AITraining {
                         }
                         plc2 = plc2 + plc;
                     }
-                        boolean xx=PatchworkGame.isPlacementValid(patchCircle, plc2);
+
                     if (State.check_turn(PatchworkGame.p1,PatchworkGame.p2)==winer){
                         int order =tileToNum(plc.charAt(0));
                         //for tile info
@@ -284,7 +298,33 @@ public class AITraining {
             e.printStackTrace();
         }
     }
-    void network01(char tile){
+    private static double[][] encoding(String patchCircle, String placement){
+        ArrayList<String> candi = generateAllPositionPlacement(patchCircle,placement);
+        State[] players = {PatchworkGame.p1,PatchworkGame.p2};
+        int turn = State.check_turn(players[0],players[1])-1;
+        double[][] result = new double[candi.size()][170];
+        for (int i=0;i<candi.size();i++){
+            PatchworkGame.isPlacementValid(patchCircle,placement);
+            int order = tileToNum(candi.get(i).charAt(0));
+            result[i][0] = order;
+            result[i][1] = PatchworkGame.tileTimetoken[order];
+            result[i][2] = PatchworkGame.tileButton[order];
+            result[i][3] = PatchworkGame.tileSpace[order].length;
+            result[i][4] = PatchworkGame.tileCost[order];
+            result[i][5] = players[turn].buttonCount;
+            result[i][6] = players[turn].timecount;
+            result[i][6] = players[turn].specialButton;
+            result[i][7] = players[turn].squareleft;
+            for (int k=0;k<9;k++)
+                for (int h =0 ;h<9;h++)
+                    result[i][k*9+h+8] = (PatchworkGame.p1.squiltBoard[k][h]?0:1);
+            for (int k=0;k<9;k++)
+                for (int h =0 ;h<9;h++)
+                    result[i][k*9+h+89] = (PatchworkGame.p2.squiltBoard[k][h]?0:1);
+        }
+        return result;
+    }
+    static void network01(char tile){
         Matrix[] nn = new Matrix[LAYERS];
         // initialise the layers
         nn[0] = new Matrix(7,12,0);
